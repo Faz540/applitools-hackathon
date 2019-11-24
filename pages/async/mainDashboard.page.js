bbe019412ac486e2cb622bb287c647f9ccfd5898
Paul Farrell <faz-mac@Pauls-MBP.connect>
2019-11-24 01:37:25 +0000

    Removed unused elements for the async page objects

--- pages/async/mainDashboard.page.js
+++ pages/async/mainDashboard.page.js
@@ -1,16 +1,10 @@
 class MainDashboard {
     // Main Dashboard Elements:
-    get $content() { return $(".content-i") };
     get $topMenu() { return $(".top-bar") }
     get $compareExpensesButton() { return $("#showExpensesChart") };
     get $finanicalOverviewSection() { return $(".element-balances") };
     get $transactionTable() { return $("#transactionsTable") };
-    get $amountTableHeader() { return $("#amount") };
-    get $$amounts() { return $$(".text-right") };
-    get $$tableRows() { return $$("#transactionsTable tbody tr") };
-    get $gifCyberMondayFlashSale() { return $("#flashSale img") };
-    get $gifFlashSale() { return $("#flashSale2 img") };
-    
+    get $amountTableHeader() { return $("#amount") };    
 };
 
 module.exports = new MainDashboard;
